7ab5320453f7746cde881d46d04450097b933fad
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-12 01:01:54 +0900

    Add: types file

---
+++ src/types/firebaseConfig.d.ts
@@ -0,0 +1,12 @@
+// TypeScript types file
+
+type FirebaseConfig = {
+  apiKey: string,
+  authDomain: string,
+  databaseURL: string,
+  projectId: string,
+  storageBucket: string,
+  messagingSenderId: string,
+  appId: string,
+  measurementId: string
+}

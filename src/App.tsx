0c2ad3755b084241db8535f64d2cf3d06cd9909c
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-12 01:43:33 +0900

    Add: firebase connect function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from 'firebase/app';
+import '@firebase/firestore';
+import { firebaseConfig } from './config/firebaseConfig';
 
 export function App() {
+  try {
+    firebase.initializeApp(firebaseConfig);
+    console.log('Firebase connect ...OK');
+  } catch (err) {
+    if(!/already exists/.test(err.message)) {
+      console.log('Firebase connection ...NO');
+    }
+  }
   return (
     <div>
       <p>Hello World</p>

645f76fd19c8ea308558442714b7ef47feae7a7c
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-14 00:41:26 +0900

    Create: base Editor component

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { debounce } from '../../util/helper';
+
+export function Editor() {
+  return (
+    <ReactQuill 
+      
+    />
+  )
+}

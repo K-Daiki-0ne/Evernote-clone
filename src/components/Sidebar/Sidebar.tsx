c04ed1604f4171cad08dee8fdd93ad3de5e0d43c
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-21 22:41:00 +0900

    Add: new note button & title update function

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -14,17 +14,39 @@ export function Sidebar<HomeProp>() {
   const [addingNote, setAddingNote] = useState<boolean>(false);
   const [title, setTitle] = useState<string>('');
 
-  // function newNote() {
-  //   console.log()
-  // }
+  function newNote() {
+    setAddingNote(!addingNote);
+    setTitle('');
+  }
 
+  function updatetitle(e: any) {
+    e.preventDefault();
+    setTitle(e.target.value);
+  }
 
   const classes: any = useStyles();
+
   return (
     <div className={classes.sidebarContainer}>
-      <Button className={classes.newNoteBtn}>
+      <Button 
+        className={classes.newNoteBtn}
+        onClick={newNote}
+      >
         New Note
       </Button>
+      {
+        addingNote ? 
+        <div>
+          <input 
+            type="text" 
+            className={classes.newNoteInput}
+            placeholder='Enter note title'
+            onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>): void => updatetitle(e)}
+          />
+        </div>
+
+        : null
+      }
     </div>
   )
 }

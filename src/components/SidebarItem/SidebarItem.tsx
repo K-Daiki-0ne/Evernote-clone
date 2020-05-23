7de330d483779606f3006858a48b0a54d229e7f7
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-23 22:12:18 +0900

    Add: connect custom style in SidebarItem

--- src/components/SidebarItem/SidebarItem.tsx
+++ src/components/SidebarItem/SidebarItem.tsx
@@ -4,9 +4,11 @@ import {
   ListItemText,
 } from '@material-ui/core';
 import DeleteIcon from '@materila-ui/icons';
+import { useStyles } from './style.js'
 import { removeHTMLTags } from '../../util/helper';
 
 export function SidebarItem() {
+  const classes: Classes = useStyles();
   return (
     <div>
       <ListItem>

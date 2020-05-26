a893bb478492bf1684e6279cd26facf331bf5dde
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-05-26 23:37:29 +0900

    Create: base SidebarItem component

--- src/components/SidebarItem/SidebarItem.tsx
+++ src/components/SidebarItem/SidebarItem.tsx
@@ -10,7 +10,7 @@ import { removeHTMLTags } from '../../util/helper';
 type SidebarItemProps = {
   _note: Note;
   _index: number;
-  selectedNoteIndex: any;
+  selectedNoteIndex: number;
   selectItem: () => void;
   deleteItem: () => void;
 }
@@ -18,13 +18,38 @@ type SidebarItemProps = {
 export const SidebarItem: React.FC<SidebarItemProps> = ({ _note, _index, selectedNoteIndex, selectItem, deleteItem }) => {
 
   const classes: Classes = useStyles();
+
+  function selectOneItem(n: Note, e: number): void {
+    selectItem(n, e);
+  };
+
+  function deleteOneItem(note: Note): void {
+    if(window.confim('Are you sure delete this one ?')) {
+      deleteItem(note);
+    };
+  };
+
+
   return (
-    <div>
-      <ListItem>
-        <div>
-          <ListItemText />
+    <div key={_index}>
+      <ListItem 
+        className={classes.listItem}
+        selected={selectedNoteIndex === _index}
+        alignItems='flex-start'
+      >
+        <div 
+          className={classes.textSection}
+          onClick={selectOneItem(_note, _index)}
+        >
+          <ListItemText 
+            primary={_note.title}
+            secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}
+          />
         </div>
-        <DeleteIcon />
+        <DeleteIcon 
+          className={classes.deleteIcon}
+          onClick={deleteOneItem(_note)}
+        />
       </ListItem>
     </div>
   )

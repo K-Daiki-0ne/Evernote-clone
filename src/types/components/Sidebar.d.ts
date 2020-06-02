478741574fb991973d44a435ce94e1fc45f92720
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-06-03 07:16:14 +0900

    Delete: types

--- src/types/components/Sidebar.d.ts
+++ src/types/components/Sidebar.d.ts
@@ -1,11 +1,1 @@
 type AddingNoteType = boolean;
-
-type NewNote = () => void;
-
-type UpdateTitle = (e: any) => void;
-
-type CreateNote = () => void;
-
-type SelectItem = () => void;
-
-type DeleteItem = () => void;
